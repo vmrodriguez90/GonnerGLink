2ab4748fa7812960963661ff038b118034f62c72
Victor Rodriguez <victormanuelrodriguez90@gmail.com>
2018-05-28 22:29:03 -0300

    New Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,26 @@
-import { NgModule } from '@angular/core';
-import { IonicApp, IonicModule } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { SmsService } from './../providers/sms-service';
-import { User } from './../providers/user';
-import {ControlPage} from './../pages/control/control';
-import {LinkPage} from './../pages/link/link';
-import {PinPage} from './../pages/pin/pin';
-import {ReportInputPage} from './../pages/report-input/report-input';
-import {TimeInputPage} from './../pages/time-input/time-input';
-import {TelephonePage} from './../pages/telephone/telephone';
-import {ProfilePage } from './../pages/profile/profile';
-import {TimeOutputPage} from './../pages/time-output/time-output';
-import {ReportOutputPage} from './../pages/report-output/report-output';
-import {StatusPage} from './../pages/status/status';
-import {InnamePage} from './../pages/inname/inname';
-import {OutnamePage} from './../pages/outname/outname';
+import {NgModule} from "@angular/core";
+import {IonicApp, IonicModule} from "ionic-angular";
+import {MyApp} from "./app.component";
+import {SmsService} from "./../providers/sms-service";
+import {User} from "./../providers/user";
+import {ControlPage} from "./../pages/control/control";
+import {LinkPage} from "./../pages/link/link";
+import {PinPage} from "./../pages/pin/pin";
+import {ReportInputPage} from "./../pages/report-input/report-input";
+import {TimeInputPage} from "./../pages/time-input/time-input";
+import {TelephonePage} from "./../pages/telephone/telephone";
+import {ProfilePage} from "./../pages/profile/profile";
+import {TimeOutputPage} from "./../pages/time-output/time-output";
+import {ReportOutputPage} from "./../pages/report-output/report-output";
+import {StatusPage} from "./../pages/status/status";
+import {InnamePage} from "./../pages/inname/inname";
+import {OutnamePage} from "./../pages/outname/outname";
+import {MaterialModule} from "@angular/material";
+import {HomePage} from "./../pages/home/home";
+import {IonicStorageModule} from "@ionic/storage";
 
-import { HomePage } from './../pages/home/home';
-import { Storage } from '@ionic/storage';
 
-
-declare var window: any;
+declare var window:any;
 
 @NgModule({
   declarations: [
@@ -38,10 +38,11 @@ declare var window: any;
     ProfilePage,
     OutnamePage,
     InnamePage
-
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    MaterialModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -62,8 +63,8 @@ declare var window: any;
   ],
   providers: [
     SmsService,
-    User,
-    Storage
+    User
   ]
 })
-export class AppModule {}
+export class AppModule {
+}
